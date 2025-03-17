04a1fa11199506755868f7625dffca34f36248e6
LazarBankov <152815037+LazarBankov@users.noreply.github.com>
2025-03-17 16:09:25 +0200

    Added basic components with routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css'
+import { Routes, Route } from 'react-router'
+
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
+import './App.css'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Catalog from './components/catalog/Catalog'
+import DetailsProduct from './components/details/DetailsProduct'
 
 function App() {
 
   return (
       <div className='container'>
         <Header />
+        <main className='main'>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/catalog/:prductId' element={<DetailsProduct/>} />
+            <Route path='/catalog' element={<Catalog/>} />
+            <Route path='/about' element={<About/>} />
+          </Routes>
+        </main>
         <Footer />
       </div>
   )
