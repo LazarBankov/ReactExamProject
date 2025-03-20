fc7b2ebc6ed0b90373ec10d0c62707e711548312
LazarBankov <152815037+LazarBankov@users.noreply.github.com>
2025-03-20 13:46:56 +0200

    made basic html catalog

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,7 +1,21 @@
+import React from 'react';
+
+
 export default function Catalog() {
     return (
-        <div>
-            Catalog
+        <div className="catalog-container">
+
+                <div  className="catalog-section">
+                    <h2>Name of section</h2>
+                    <div className="articles-container">
+
+                            <div  className="article-item">
+                                <img  className="article-image" />
+                                <h3>Article name</h3>
+                                <button className="details-button">Details</button>
+                            </div>
+                    </div>
+                </div>
         </div>
-    )
+    );
 }
