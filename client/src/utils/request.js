01a2c11ccc5ff6436377bf72047eacf36c556d59
LazarBankov <152815037+LazarBankov@users.noreply.github.com>
2025-03-20 11:17:39 +0200

    Made requestTemplate hook

---
+++ client/src/utils/request.js
@@ -0,0 +1,36 @@
+const requestTemplate = async (method, url, data, options = {}) => {
+    if (method !== 'GET') {
+        options.method = method;
+    }
+
+    if (data) {
+        options = {
+            ...options,
+            headers: {
+                'Content-Type': 'application/json',
+                ...options.headers,
+            },
+            body: JSON.stringify(data),
+        }
+    }
+
+    const response = await fetch(url, options);
+    const responseContentType = response.headers.get('Content-Type');
+    if (!responseContentType) {
+        return;
+    }
+    
+    const result = await response.json();
+
+    return result;
+
+};
+
+export default {
+    get: requestTemplate.bind(null, 'GET'),
+    // get: (...params) => request('GET', ...params)
+    post: requestTemplate.bind(null, 'POST'),
+    put: requestTemplate.bind(null, 'PUT'),
+    delete: requestTemplate.bind(null, 'DELETE'),
+    baseRequest: requestTemplate,
+}
