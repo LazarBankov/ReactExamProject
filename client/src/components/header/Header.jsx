e86b48278437029a8bce5dd1aeda84f26c21178d
LazarBankov <152815037+LazarBankov@users.noreply.github.com>
2025-03-30 18:37:37 +0300

    Updated the navigation via authorizated or not

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,31 +1,32 @@
 import { Link } from 'react-router';
 import styles from './Header.module.css';
+import  useAuthHook  from '../../hooks/useAuthHook';
 
 export default function Header() {
+
+    const { isAuthenticated } = useAuthHook();
+    
     return (
         <header className={styles.header}>
             <Link to="/">
                 <img src="/CvetniDekoraciiLogo.png" alt="Logo" className={styles.logo} />
             </Link>
-            <ul className={styles.navList}>
-                <li className={styles.navItem}>
-                    <Link to="/catalog">Catalog</Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link to="/about">About</Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link to="/register">Register</Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li className={styles.navItem}>
+            <nav className={styles.navList}>
+                {isAuthenticated ?(
+                <li className={styles.navList}>
                     <Link to="/cart/:userId">
                         <i className="fas fa-shopping-cart"></i>
                     </Link>
                 </li>
-            </ul>
+                ) : (
+                <li className={styles.navList}>
+                    <Link  to="/catalog">Catalog</Link>
+                    <Link to="/register">Register</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/about">About</Link>
+                </li> )
+                }
+            </nav>
         </header>
     ) 
 }
