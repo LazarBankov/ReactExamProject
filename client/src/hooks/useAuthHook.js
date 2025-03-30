e1522436aa5faffa6bdb4534addd8494bdbf2122
LazarBankov <152815037+LazarBankov@users.noreply.github.com>
2025-03-30 18:36:11 +0300

    Created hook for authorization

---
+++ client/src/hooks/useAuthHook.js
@@ -0,0 +1,34 @@
+import { useContext, useMemo, useCallback } from "react";
+import { UserContext } from "../contexts/Context";
+import request from "../utils/request";
+
+export default function useAuthHook() {
+    const { accessToken, ...authData } = useContext(UserContext);
+
+    const requestWrapper = useCallback((method, url, data, options = {}) => {
+        const authOptions = {
+            ...options,
+            headers: {
+                'X-Authorization': accessToken,
+                ...options.headers
+            }
+        };
+
+        return request.baseRequest(method, url, data, accessToken ? authOptions : options);
+    }, [accessToken]);
+
+    const requestObject = useMemo(() => ({
+        get: requestWrapper.bind(null, 'GET'),
+        post: requestWrapper.bind(null, 'POST'),
+        put: requestWrapper.bind(null, 'PUT'),
+        delete: requestWrapper.bind(null, 'DELETE'),
+    }), [requestWrapper])
+
+    return {
+        ...authData,
+        accessToken,
+        userId: authData._id,
+        isAuthenticated: !!accessToken,
+        request: requestObject,
+    }
+}
