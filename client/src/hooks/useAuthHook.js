2052af339b7845ca108c00be980eaf9238d5e920
LazarBankov <152815037+LazarBankov@users.noreply.github.com>
2025-04-09 11:52:29 +0300

    Made the logic of isAdmin and made code more readeble

--- client/src/hooks/useAuthHook.js
+++ client/src/hooks/useAuthHook.js
@@ -3,32 +3,45 @@ import { UserContext } from "../contexts/Context";
 import request from "../utils/request";
 
 export default function useAuthHook() {
-    const { accessToken, ...authData } = useContext(UserContext);
+  const { accessToken, ...authData } = useContext(UserContext);
+  const isAdmin = authData.email === "admin@abv.bg";
 
-    const requestWrapper = useCallback((method, url, data, options = {}) => {
-        const authOptions = {
-            ...options,
-            headers: {
-                'X-Authorization': accessToken,
-                ...options.headers
-            }
-        };
+  const requestWrapper = useCallback(
+    (method, url, data, options = {}) => {
+      const authOptions = {
+        ...options,
+        headers: {
+          "X-Authorization": accessToken,
+          ...options.headers,
+        },
+      };
 
-        return request.baseRequest(method, url, data, accessToken ? authOptions : options);
-    }, [accessToken]);
+      return request.baseRequest(
+        method,
+        url,
+        data,
+        accessToken ? authOptions : options
+      );
+    },
+    [accessToken]
+  );
 
-    const requestObject = useMemo(() => ({
-        get: requestWrapper.bind(null, 'GET'),
-        post: requestWrapper.bind(null, 'POST'),
-        put: requestWrapper.bind(null, 'PUT'),
-        delete: requestWrapper.bind(null, 'DELETE'),
-    }), [requestWrapper])
+  const requestObject = useMemo(
+    () => ({
+      get: requestWrapper.bind(null, "GET"),
+      post: requestWrapper.bind(null, "POST"),
+      put: requestWrapper.bind(null, "PUT"),
+      delete: requestWrapper.bind(null, "DELETE"),
+    }),
+    [requestWrapper]
+  );
 
-    return {
-        ...authData,
-        accessToken,
-        userId: authData._id,
-        isAuthenticated: !!accessToken,
-        request: requestObject,
-    }
+  return {
+    ...authData,
+    accessToken,
+    isAdmin,
+    userId: authData._id,
+    isAuthenticated: !!accessToken,
+    request: requestObject,
+  };
 }
